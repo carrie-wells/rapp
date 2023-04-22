2ef4dad3537f23bfc0d9c5b89b3c9f56955042a8
Carrie Wells <carrwell@gmail.com>
2023-04-21 22:33:35 -0400

    fix

--- pages/thanks.js
+++ pages/thanks.js
@@ -1,6 +1,5 @@
 function TheApp() {
-    return <h1>Thanks for your order!</h1>
-    return <h2>We hope you enjoy your food</h2>
+    return <h1>Thanks for your order. We appreciate your business!</h1>
   }
   
   export default TheApp
